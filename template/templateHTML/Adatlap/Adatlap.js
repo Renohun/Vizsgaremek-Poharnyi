2737ba532984c7853f04d680318dcefd1c455129
BenczikMarcell <benczikmarcellferenc@gmail.com>
2025-10-07 18:21:58 +0200

    Adatlap Munkák

---
+++ template/templateHTML/Adatlap/Adatlap.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    let adat=document.getElementById("Adatlap")
+    let fav=document.getElementById("Kedvencek")
+    let koktel=document.getElementById("Koktélok")
+    let report=document.getElementById("Jelentések")
+
+    let adatoldal=document.getElementById("adatoldal")
+    let kedvoldal=document.getElementById("kedvencoldal")
+    let koktoldal=document.getElementById("kokteloldal")
+    let repoldal=document.getElementById("reportoldal")
+    adat.addEventListener("click",()=>{
+        adatoldal.removeAttribute("hidden","true")
+        kedvoldal.setAttribute("hidden","true")
+        koktoldal.setAttribute("hidden","true")
+        repoldal.setAttribute("hidden","true")
+    })
+    fav.addEventListener("click",()=>{
+        adatoldal.setAttribute("hidden","true")
+        kedvoldal.removeAttribute("hidden","true")
+        koktoldal.setAttribute("hidden","true")
+        repoldal.setAttribute("hidden","true")
+    })
+    koktel.addEventListener("click",()=>{
+        adatoldal.setAttribute("hidden","true")
+        kedvoldal.setAttribute("hidden","true")
+        koktoldal.removeAttribute("hidden","true")
+        repoldal.setAttribute("hidden","true")
+    })
+    report.addEventListener("click",()=>{
+        adatoldal.setAttribute("hidden","true")
+        kedvoldal.setAttribute("hidden","true")
+        koktoldal.setAttribute("hidden","true")
+        repoldal.removeAttribute("hidden","true")
+    })
+})
