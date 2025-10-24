15b5df67063f9e9738cccc118d4668e10632948a
Renohun <127425333+Renohun@users.noreply.github.com>
2025-10-24 15:07:24 +0200

    Maradek weboldal osszekotve
    no kommnet

--- template/templateHTML/Navbar/NavBar.js
+++ template/templateHTML/Navbar/NavBar.js
@@ -1,11 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
     const belepesGomb = document.getElementById('Login');
     const regisztraciosGomb = document.getElementById('Register');
+    const koktelKeszites = document.getElementById('Keszites');
+    const profilKep = document.getElementById('Profile');
 
     belepesGomb.addEventListener('click', () => {
         window.location.href = '../belepes/index.html';
     });
     regisztraciosGomb.addEventListener('click', () => {
         window.location.href = '../Regisztráció/Regisztracio.html';
+    });
+    koktelKeszites.addEventListener('click', () => {
+        window.location.href = '../Új Koktél/NewCocktail.html';
+    });
+    profilKep.addEventListener('click', () => {
+        window.location.href = '../Adatlap/Adatlap.html';
     });
 });
