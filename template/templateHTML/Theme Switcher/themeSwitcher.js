8e4375c5dac398e6392211b3e4cc7ceeee97ce36
Renohun <127425333+Renohun@users.noreply.github.com>
2025-10-30 12:48:23 +0100

    themeSwitcher tovabbfejlesztese
    Megcsinilva, hogy a minden weboldalon megtortenjen a tema valtas. Majd ez a localStorage kell majd hasznalnunk a navbarhoz is kesobb

--- template/templateHTML/Theme Switcher/themeSwitcher.js
+++ template/templateHTML/Theme Switcher/themeSwitcher.js
@@ -1,6 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
     const themeSwitcherBtn = document.getElementById('ThemeSwitcher');
 
+    //Ugyanaz a logikai maradt
+
+    //Ez a resz azert felel hogy a weboldal betoltese utan megnezze hogy van e valami elmentve a localstorageba
+    //LocalStorage ugy mukodik mint egy objektum, van egy property-e es egy value-ja
+    //.getItem - lehet rakeresni az adott Item property alapjan
+    //.setItem - ezzel allitjuk be a property erteket
+    //.removeItem - adja magat
+    const savedTheme = localStorage.getItem('Theme');
+    console.log(savedTheme);
+    if (savedTheme != null) {
+        const linkTags = document.getElementsByTagName('link');
+        for (let i = 0; i < linkTags.length; i++) {
+            //Igy szebben is lehet megcsinalni
+            //Ez csak a utolso fontos dolgokat adja vissza, azzal megoldassal ellentetben mint ahogyan csinaltam korabban
+            const href = linkTags[i].getAttribute('href');
+            console.log(href);
+            if (href.includes('lightTheme.css') && savedTheme === 'DarkTheme') {
+                console.log('light if');
+                linkTags[i].setAttribute('href', href.replace('lightTheme.css', 'darkTheme.css'));
+            } else if (href.includes('darkTheme.css') && savedTheme === 'LightTheme') {
+                console.log('dark if');
+                linkTags[i].setAttribute('href', href.replace('darkTheme.css', 'lightTheme.css'));
+            }
+        }
+    }
+
     themeSwitcherBtn.addEventListener('click', () => {
         const currentTheme = document.getElementsByTagName('link');
         hrefArr = [];
@@ -32,10 +58,15 @@ document.addEventListener('DOMContentLoaded', () => {
             folderPath += 'darkTheme.css';
             console.log(folderPath);
             currentTheme[poz].setAttribute('href', folderPath);
+            //Itt mentem el a azt hogy mire valtottam a temat, igy majd ha script megleszhivva ujra, akkor mar lesz mentve a localstorage-ba igy mi majd meglesz nezve hogy mi van elmentve
+            localStorage.setItem('Theme', 'DarkTheme');
         } else {
             folderPath += 'lightTheme.css';
             console.log(folderPath);
             currentTheme[poz].setAttribute('href', folderPath);
+
+            // detto
+            localStorage.setItem('Theme', 'LightTheme');
         }
     });
 });
