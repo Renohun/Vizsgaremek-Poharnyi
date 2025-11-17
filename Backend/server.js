87714d9e59b36f5a9559bf9a5c46ebc136df52af
Renohun <127425333+Renohun@users.noreply.github.com>
2025-11-17 13:39:19 +0100

    api vegpont felallitasa
    most a szerver kepes API endpointot fogadasara  vagy valami hasonlo

--- Backend/server.js
+++ Backend/server.js
@@ -4,11 +4,18 @@ const port = 8000;
 const ip = '127.0.0.1';
 
 const server = express();
-//A mappa betoltese
+server.use(express.json());
+
+//Korabban toltse be a API-t mint a statikus oldalt
+const apiRoutes = require('./api/api');
+server.use('/api', apiRoutes);
+
+//Mindent toltson be a Frontend mappabol, a css/bootstrap miatt
 server.use(express.static(path.join(__dirname, '..', 'Frontend', 'templateHTML')));
-//Landing
-server.get('/', (request, response) => {
-    response.sendFile(path.join(__dirname, '..', 'Frontend', 'templateHTML', 'Landing Page', 'Landing Page.html'));
+
+//szerver futattas eseten a landing page legyen az elso html amit megnyit
+server.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'Frontend', 'templateHTML', 'Landing Page', 'Landing Page.html'));
 });
 
 server.listen(port, ip, () => {
