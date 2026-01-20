7a52abcfcff204612e6da525868ddee5e61cad72
Renohun <127425333+Renohun@users.noreply.github.com>
2026-01-20 22:30:00 +0100

    authorizationMiddelware - extra vedelem
    Hozzaadtam egy extra ellenorzest, hogy meg ha valaki dekodolja a sutit akkor ne tudjon nagyon betorni az admin panelbe

--- Backend/api/authorizationMiddelware.js
+++ Backend/api/authorizationMiddelware.js
@@ -1,11 +1,22 @@
-function AuthorizaitionMiddleware(req,res,next){
-   if(req.data && req.data.adminStatus == 1){
-        next()
-   }
-   else{
-        res.status(403).json({
-            message: "Nincs jogod"
-        })
-   }
+const DBconnetion = require('../database.js');
+
+function AuthorizaitionMiddleware(req, res, next) {
+    const query = 'SELECT Admin FROM felhasználó WHERE FelhID LIKE ?';
+    //console.log(req.data.userID);
+
+    DBconnetion.query(query, [req.data.userID], (err, rows) => {
+        if (err) {
+            res.status(500).json({ message: 'Hibas adatbazis eleres' });
+        }
+        //console.log(rows[0].Admin);
+
+        if (req.data && req.data.adminStatus == 1 && rows[0].Admin == 1) {
+            next();
+        } else {
+            res.status(403).json({
+                message: 'Nincs jogod'
+            });
+        }
+    });
 }
-module.exports = AuthorizaitionMiddleware
+module.exports = AuthorizaitionMiddleware;
