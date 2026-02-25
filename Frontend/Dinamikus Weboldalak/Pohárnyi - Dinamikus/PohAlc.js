180c3823a67216724da8121c2b0fb725c3a5e3d1
Renohun <127425333+Renohun@users.noreply.github.com>
2026-02-25 20:41:25 +0100

    Nev alapu kereses -- javitva
    Megcsinalva, hogy le is rendelje a kokteleokat es ne csak a logolja oket

--- Frontend/Dinamikus Weboldalak/Pohárnyi - Dinamikus/PohAlc.js
+++ Frontend/Dinamikus Weboldalak/Pohárnyi - Dinamikus/PohAlc.js
@@ -233,7 +233,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const keresettKoktelNeve = document.getElementById('searchBar').value;
             if (keresettKoktelNeve.length > 0) {
                 const koktelok = await GETfetch(`http://127.0.0.1:3000/api/Koktelok/lekeres/${keresettKoktelNeve}`);
-                console.log(koktelok);
+                koktelRendereles(koktelok);
             } else {
                 alert('Nem adott meg nevet!');
             }
