f31c41583f564be7ecae1c0afc179b6222207f5b
sziklaidavid <davidhoki10@gmail.com>
2026-03-06 10:47:37 +0100

    Webshop Main bugfix
    slider frisssítése már a textbox kitöltésével is megtörténik

--- Frontend/Dinamikus Weboldalak/WebShopMain/WebShopMain.js
+++ Frontend/Dinamikus Weboldalak/WebShopMain/WebShopMain.js
@@ -41,17 +41,24 @@ const Sliderek = async () => {
     output2.value = alk[0];
     slider2.setAttribute('min', alk[0]);
     slider2.setAttribute('max', alk[alk.length - 1]);
+
+    slider1.addEventListener('input', () => {
+        output1.value = slider1.value;
+    });
+    slider2.addEventListener('input', () => {
+        output2.value = slider2.value;
+    });
     //eventlistener
     output1.addEventListener('change', () => {
         //minimum maximum érték ellenörzése
         if (output1.value < ar[0]) {
             output1.value = ar[0];
-            slider1.setAttribute('value',  ar[0]);
+            slider1.value = ar[0];
         } else if (output1.value > ar[ar.length - 1]) {
             output1.value = ar[ar.length - 1];
-            slider1.setAttribute('value', ar[ar.length - 1]);
+            slider1.value = ar[ar.length - 1];
         } else {
-            slider1.setAttribute('value', output1.value);
+            slider1.value = output1.value;
             console.log(slider1.value);
         }
     });
@@ -59,59 +66,68 @@ const Sliderek = async () => {
         //alkohol %-nál
         if (output2.value < alk[0]) {
             output2.value = alk[0];
-            slider2.setAttribute('value', alk[0]);
+            slider2.value = alk[0];
         } else if (output2.value > alk[alk.length - 1]) {
             output2.value = alk[alk.length - 1];
-              slider2.setAttribute('value', alk[0]);
+            slider2.value = alk[0];
         } else {
-            slider2.setAttribute('value', output2.value);
+            slider2.value = output2.value;
             console.log(slider2.value);
         }
     });
-    slider1.oninput = function () {
-        output1.value = this.value;
-    };
-    slider2.oninput = function () {
-        output2.value = this.value;
-    };
 };
-const SzarmazasSelectFeltolt = (data,Select1, Select2)=>{
-let alapOption1 = document.createElement("option")
-alapOption1.setAttribute("value","-")
-alapOption1.setAttribute("id","AlapOrszagOption")
-alapOption1.innerHTML = "-"
-Select1.appendChild(alapOption1)
-let alapOption2 = document.createElement("option")
-alapOption2.setAttribute("value","-")
-alapOption2.setAttribute("id","AlapMarkaOption")
-alapOption2.innerHTML = "-"
-Select2.appendChild(alapOption2)
-for (let i = 0; i < data.data.length; i++) 
-{
-//orszagSelect
-   let option = document.createElement("option")
-   option.setAttribute('value',data.data[i].TermekSzarmazas) 
-   option.innerHTML = data.data[i].TermekSzarmazas;
-   option.setAttribute("id",`szarmazas${i}`)
-   Select1.appendChild(option)
-//MárkaSelect
-   let option2 = document.createElement("option")
-   option2.setAttribute('value',data.data[i].TermekMarka) 
-   option2.innerHTML = data.data[i].TermekMarka;
-   option2.setAttribute("id",`Marka${i}`)
-   Select2.appendChild(option2)
-}
-}
+const SelectFeltolt = (data, Select1, Select2, Select3) => {
+    let alapOption1 = document.createElement('option');
+    alapOption1.setAttribute('value', '-');
+    alapOption1.setAttribute('id', 'AlapOrszagOption');
+    alapOption1.innerHTML = '-';
+    Select1.appendChild(alapOption1);
+    //2.Option
+    let alapOption2 = document.createElement('option');
+    alapOption2.setAttribute('value', '-');
+    alapOption2.setAttribute('id', 'AlapMarkaOption');
+    alapOption2.innerHTML = '-';
+    Select2.appendChild(alapOption2);
+    //3.Option
+    let alapOption3 = document.createElement('option');
+    alapOption3.setAttribute('value', '-');
+    alapOption3.setAttribute('id', 'AlapKategOption');
+    alapOption3.innerHTML = '-';
+    Select3.appendChild(alapOption3);
+
+    let kategoria = [];
+    for (let i = 0; i < data.data.length; i++) {
+        //orszagSelect
+        let option = document.createElement('option');
+        option.setAttribute('value', data.data[i].TermekSzarmazas);
+        option.innerHTML = data.data[i].TermekSzarmazas;
+        option.setAttribute('id', `szarmazas${i}`);
+        Select1.appendChild(option);
+        //MárkaSelect
+        let option2 = document.createElement('option');
+        option2.setAttribute('value', data.data[i].TermekMarka);
+        option2.innerHTML = data.data[i].TermekMarka;
+        option2.setAttribute('id', `Marka${i}`);
+        Select2.appendChild(option2);
+        //KategSelect
+
+        let option3 = document.createElement('option');
+        option2.setAttribute('value', data.data[i].TermekMarka);
+        option2.innerHTML = data.data[i].TermekMarka;
+        option2.setAttribute('id', `Marka${i}`);
+        Select2.appendChild(option2);
+    }
+};
 
 document.addEventListener('DOMContentLoaded', async () => {
     const data = await TermekLekeres('/api/WebShop/TermekLekeres');
     console.log(data);
     Sliderek();
     //Selectek feltöltése
-    let OrszagSelect = document.getElementById("OrszagSelect")
-    let MarkaSelect = document.getElementById("MarkaSelect")
-    SzarmazasSelectFeltolt(data,OrszagSelect,MarkaSelect)
-    OrszagSelect.addEventListener("change",()=>{
-        console.log(OrszagSelect.value)
-    })
+    let OrszagSelect = document.getElementById('OrszagSelect');
+    let MarkaSelect = document.getElementById('MarkaSelect');
+    SelectFeltolt(data, OrszagSelect, MarkaSelect);
+    OrszagSelect.addEventListener('change', () => {
+        console.log(OrszagSelect.value);
+    });
 });
