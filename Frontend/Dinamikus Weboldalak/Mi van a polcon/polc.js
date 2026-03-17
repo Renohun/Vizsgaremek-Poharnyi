8e5442f71a2cc29c2ae002aab1dda9231a3a54d8
BenczikMarcell <benczikmarcellferenc@gmail.com>
2026-03-17 15:59:07 +0100

    refactoring

--- Frontend/Dinamikus Weboldalak/Mi van a polcon/polc.js
+++ Frontend/Dinamikus Weboldalak/Mi van a polcon/polc.js
@@ -24,49 +24,44 @@ let opciokLista=document.getElementById("ideOpciok")
 
 
 document.addEventListener("DOMContentLoaded",()=>{
+    //A koktélok összevetőit tartalmazó lista
     let lista=["Whiskey","Tequilla","Tonic"]
+    //A suggestion tömb létrehozása, és feltöltése
     let valasztek=document.createElement("div")
+    //elemenként
     for (let i = 0; i < lista.length; i++) {
-                let valasz=document.createElement("input")
-                valasz.setAttribute("type","button")
-                valasz.classList.add("ogomb")
-                valasz.addEventListener("click",()=>{
-                    osszetevoInput.value=lista[i]
-                    opciokLista.removeChild(valasztek)
-                    opciokLista.setAttribute("hidden","")
-                })
-                valasz.setAttribute("value",lista[i])
-                valasztek.appendChild(valasz)
-        }
+            //létrehozunk egy új elemet
+            ossztetevoOpcio(lista[i],valasztek)
+    }
+    //Ha rámegyünk a szövegmezőre
     osszetevoInput.addEventListener("focusin",()=>{
-        
+        //Megjeleníjük a választható összetevőket
         opciokLista.removeAttribute("hidden")
         opciokLista.appendChild(valasztek)
     })
-
+    //Ha
+    opciokLista.addEventListener("",()=>{
+        //Elrejtük a választható összetevőket
+        opciokLista.setAttribute("hidden","")
+        opciokLista.removeChild(valasztek)
+    })
+    //Amikor gépelünk
     osszetevoInput.addEventListener("input",()=>{
+        //Eltüntetjük és kiürítjük az összetevők listáját
         opciokLista.removeChild(valasztek)
         valasztek.innerHTML=""
-        let hanydb=0
+        //Ez a változó arra szolgál, hogy megtudjuk van e olyan összetevő ami megfelel a feltételeknek
+        let vane=false
+        //Végigmegyünk az összetevőkön
         for (let i = 0; i < lista.length; i++) {
-
+            //Ha van olyan elem, ami a felhasználó által leírt szöveggel kezdődik, és még nincs kiválasztva
             if (lista[i].toLowerCase().startsWith(osszetevoInput.value.toLowerCase())&&felhosszetevok.includes(lista[i])==false) {
-                
-                let valasz=document.createElement("input")
-                valasz.setAttribute("type","button")
-                valasz.classList.add("ogomb")
-                valasz.addEventListener("click",()=>{
-                    osszetevoInput.value=lista[i]
-                    opciokLista.removeChild(valasztek)
-                    opciokLista.setAttribute("hidden","")
-                    hozzaadasGomb.removeAttribute("disabled","")
-                })
-                valasz.setAttribute("value",lista[i])
-                valasztek.appendChild(valasz)
-                hanydb++
+                //Létrehozzuk mint opció
+                ossztetevoOpcio(lista[i],valasztek)
+                vane=true
             }
         }
-        if (hanydb==0) {
+        if (vane==false) {
             valasztek.innerHTML="Nincs ilyen összetevőnk"
             hozzaadasGomb.setAttribute("disabled","")
         }
@@ -80,7 +75,6 @@ document.addEventListener("DOMContentLoaded",()=>{
 osszetevoInput.addEventListener("input",()=>{
     if (osszetevoInput.value.length==0) {
         hozzaadasGomb.setAttribute("disabled","")
-        
     }
     else{
         hozzaadasGomb.removeAttribute("disabled","")
@@ -91,7 +85,26 @@ hozzaadasGomb.addEventListener("click",()=>{
     if (osszetevoInput.value!="") {
         badge(osszetevoInput.value)
         console.log(felhosszetevok);
-        
         osszetevoInput.value=""
     }
 })
+
+
+
+function ossztetevoOpcio(nev,div){
+    //Adunk egy gombot
+    let valasz=document.createElement("input")
+    valasz.setAttribute("type","button")
+    valasz.classList.add("ogomb")
+    //Amire rákattintva
+    valasz.addEventListener("click",()=>{
+        //belerakjuk a input mezőbe és kiszedjük a listából
+        osszetevoInput.value=nev
+        opciokLista.removeChild(div)
+        opciokLista.setAttribute("hidden","")
+    })
+    //A gomb szövege a lista i-edik elemének a tartalma lesz
+    valasz.setAttribute("value",nev)
+    //Hozzáadjuk a tömbhöz
+    div.appendChild(valasz)
+}
