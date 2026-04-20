a25b1fb0f3dfe7bbaf0db030bed4c23a4fc9e4c9
BenczikMarcell <benczikmarcellferenc@gmail.com>
2026-04-20 14:52:18 +0200

    Régi Weblapok Compressziója

--- Frontend/templateHTML/belepes/jelszomutatas.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    let checkButton = document.getElementById('jelszomutatas');
-    const jelszoInput = document.getElementById('jelszo');
-
-    checkButton.addEventListener('change', function () {
-        checkButton.checked ? jelszoInput.setAttribute('type', 'text') : jelszoInput.setAttribute('type', 'password');
-    });
-});
